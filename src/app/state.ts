31d683e7cd1f28100c7f48dea5a00b2288d50bb8
greinisch <greinisch@youwol.com>
2021-08-17 17:36:15 +0200

    :zap: AppState=> add user info

--- src/app/state.ts
+++ src/app/state.ts
@@ -1,15 +1,32 @@
-import { Subject } from "rxjs"
-import { share, tap } from "rxjs/operators"
-import { AssetsBrowserClient, Group } from "./assets-browser.client"
+import { createObservableFromFetch } from "@youwol/flux-core"
+import { AssetsGatewayClient, GroupResponse } from "@youwol/flux-youwol-essentials"
+import { BehaviorSubject, ReplaySubject } from "rxjs"
+import { map, tap } from "rxjs/operators"
+
 
 export class AppState{
 
-    selectedGroup$ : Subject<Group>
-    groups$ = AssetsBrowserClient.getGroups$().pipe(
-        share(),
-        tap( grps => this.selectedGroup$.next(grps[0]))
-    )
+    selectedGroup$ = new ReplaySubject<GroupResponse>()
+    groups$ = new BehaviorSubject<GroupResponse[]>([])
+    user$ = new ReplaySubject<any>()
 
     constructor(){
+        let client = new AssetsGatewayClient()
+
+        let requestUserInfo = new Request(
+            `/api/assets-gateway/user-info`
+        );
+        createObservableFromFetch( requestUserInfo).subscribe( resp => 
+            console.log("User info", resp)
+        )
+        client.getGroups().pipe(
+            map( ({groups}) => groups.filter(grp => !grp.id.includes('private'))),
+            tap( (groups) => this.selectedGroup$.next(groups[0]))
+        ).subscribe( grps => this.groups$.next(grps))
+    }
+
+    selectGroup(grpId: string){
+        let grp = this.groups$.getValue().find( grp => grp.id == grpId)
+        this.selectedGroup$.next(grp)
     }
 }
